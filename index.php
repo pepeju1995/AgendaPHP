ed7a85bde9dd73f6061ebbe8b57f135d3852599c
pepeju1995 <pepeju95@gmail.com>
2022-02-28 19:51:49 +0100

    # Switch to PHP

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+  $contacts = [
+    ["name" => "Pepe", "phone_number" => "2132139"],
+    ["name" => "Nate", "phone_number" => "5498794"],
+    ["name" => "Antonio", "phone_number" => "7897896"]
+  ]
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -59,38 +67,18 @@
     <main>
       <div class="container pt-4 p-3">
         <div class="row">
+        <?php foreach($contacts as $contact):?>
           <div class="col-md-4 mb-3">
             <div class="card text-center">
               <div class="card-body">
-                <h3 class="card-title text-capitalize">Contact Name 1</h3>
-                <p class="m-2">987654321</p>
+                <h3 class="card-title text-capitalize"><?=$contact["name"]?></h3>
+                <p class="m-2"><?=$contact["phone_number"]?></p>
                 <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
                 <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
               </div>
             </div>
           </div>
-  
-          <div class="col-md-4 mb-3">
-            <div class="card text-center">
-              <div class="card-body">
-                <h3 class="card-title text-capitalize">Contact Name 2</h3>
-                <p class="m-2">987654321</p>
-                <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-                <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
-              </div>
-            </div>
-          </div>
-  
-          <div class="col-md-4 mb-3">
-            <div class="card text-center">
-              <div class="card-body">
-                <h3 class="card-title text-capitalize">Contact Name 3</h3>
-                <p class="m-2">987654321</p>
-                <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-                <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
-              </div>
-            </div>
-          </div>
+        <?php endforeach ?>
         </div>
       </div>
     </main>
