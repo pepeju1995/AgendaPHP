96c15b0a4bd4db1092c8311a2f37c821451bc0d9
pepeju1995 <pepeju95@gmail.com>
2022-03-01 20:25:08 +0100

    # Delete contact

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+
+require "db.php";
+var_dump($_GET);
+$id = $_GET["id"];
+
+$stmt = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$stmt->execute([":id" => $id]);
+
+if($stmt->rowCount() == 0){
+  http_response_code(404);
+  echo("HTTP 404 NOT FOUND");
+  return;
+}
+
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
+
+header('Location: index.php');
+?>
